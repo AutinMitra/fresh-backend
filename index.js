d821e473a4c905ce487ec85fb199f25a5963ef5b
Autin Mitra <autin123@gmail.com>
2021-04-11 01:42:35 -0400

    small cors changes for dev

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const api = require('./routes/api')
 const initDB = require('./db/db')
+const cors = require('cors');
 
 const app = express()
 initDB()
 
-// app.use(require('body-parser'))
+if (!process.env.PRODUCTION)
+  app.use(cors())
 app.use('/api', api)
 
 app.listen(8080)
